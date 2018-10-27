9b0957ec3ae0a76551587902d848dbf808de31ee
tzyLee <b06901145@ntu.edu.tw>
2018-10-28 05:13:03 +0800

    Add LineChart Basis

---
+++ src/js/LineChart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {FlexibleWidthXYPlot, XAxis, YAxis, LabelSeries, LineSeries} from 'react-vis';
+
+class LineChart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        
+    );
+  }
+}
+
+export default LineChart;
