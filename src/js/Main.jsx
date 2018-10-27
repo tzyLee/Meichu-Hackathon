0cf4ea9a53fe2f3f3f1d31ce961fd716d0b9c683
tzyLee <b06901145@ntu.edu.tw>
2018-10-27 20:43:00 +0800

    Add proper name

--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -4,17 +4,14 @@ import BarChart from './BarChart.jsx';
 
 let chartData = [
   {x: 100, y: "CO2"},
-  {x: 112, y: "Temp"},
-  {x: 230, y: "Moisture"},
-  {x: 268, y: "Apr"},
-  {x: 300, y: "May"},
-  {x: 310, y: "Jun"},
-  {x: 315, y: "Jul"},
-  {x: 340, y: "Aug"},
-  {x: 388, y: "Sep"},
-  {x: 404, y: "..."},
-  {x: 442, y: "Nov"},
-  {x: 447, y: "Dec"}
+  {x: 112, y: "CO"},
+  {x: 230, y: "HCHO"},
+  {x: 268, y: "TVOC"},
+  {x: 300, y: "Bacteria"},
+  {x: 310, y: "Fungi"},
+  {x: 315, y: "PM10"},
+  {x: 340, y: "PM2.5"},
+  {x: 388, y: "O3"}
 ]
 
 class Main extends React.Component {
