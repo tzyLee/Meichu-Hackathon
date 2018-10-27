9cbc76e7d4e7c4380c017e58bd91f713bae5ff5c
byronhsu <byronhsu1230@gmail.com>
2018-10-27 16:54:00 +0800

    fix server reload bug

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ import os
 
 public = os.path.abspath('public/')
 app = Flask(__name__, template_folder = public, static_folder = public, static_url_path= '')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 3000)
--- src/js/Main.jsx
+++ src/js/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Gauges from './Gauges.jsx';
 import BarChart from './BarChart.jsx';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 let chartData = [
   {x: 100, y: "CO2"},
@@ -17,19 +20,50 @@ let chartData = [
   {x: 447, y: "Dec"}
 ]
 
+const styles = theme => ({
+   root: {
+     flexGrow: 1,
+   },
+   paper: {
+     padding: theme.spacing.unit * 2,
+     textAlign: 'center',
+     color: theme.palette.text.secondary,
+   },
+ });
+
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.classes = props;
   }
-
   render() {
     return (
-      <div>
-        <p>Test</p>
-        <Gauges></Gauges>
-        <BarChart data={chartData}></BarChart>
+      <div className = {this.classes.root}>
+         <Grid container spacing={24}>
+            <Grid item xs={12}>
+               <Paper className={this.classes.paper}>xs=12</Paper>
+            </Grid>
+            <Grid item xs={6}>
+               <Paper className={this.classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs={6}>
+               <Paper className={this.classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs={3}>
+               <Paper className={this.classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+               <Paper className={this.classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+               <Paper className={this.classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+               <Paper className={this.classes.paper}>xs=3</Paper>
+            </Grid>
+         </Grid>
       </div>);
   }
 }
 
-export default Main;
+export default withStyles(styles)(Main);
