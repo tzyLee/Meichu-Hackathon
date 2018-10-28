c420e27f821cf90c74d2a0060f6d5d78b95191e7
tzyLee <b06901145@ntu.edu.tw>
2018-10-28 09:38:18 +0800

    Add Area Series

--- src/js/Or.jsx
+++ src/js/Or.jsx
@@ -3,10 +3,12 @@ import Grid from '@material-ui/core/Grid';
 // import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Logo from '../assets/logo.png';
+import {FlexibleWidthXYPlot, XAxis, YAxis, AreaSeries} from 'react-vis';
 
 class OR extends React.Component {
   constructor(props) {
     super(props);
+    this.indoor = [{x: 1, y: 70}, {x: 2, y: 74}, {x: 3, y: 73}, {x: 4, y: 79}, {x: 5, y: 77}, {x: 6, y: 80}, {x: 7, y: 83}, {x: 8, y: 77}, {x: 9, y: 82}, {x: 10, y: 75}, {x: 11, y: 72}, {x: 12, y: 66}]
   }
 
   render() {
@@ -14,6 +16,11 @@ class OR extends React.Component {
     <React.Fragment>
       <Grid item xs={8} style={{height: '30%'}}>
         <Paper style = {{height : '100%', position: 'relative', textAlign: 'center', paddingTop: '30px'}}>
+          <FlexibleWidthXYPlot margin={{left: 50, right: 20}} height={220} xDomain={[1, 12]} yDomain={[66, 83]}>
+            <XAxis/>
+            <YAxis/>
+            <AreaSeries color='#4d4c5f' data={this.indoor}/>
+          </FlexibleWidthXYPlot>
         </Paper> 
       </Grid>
       <Grid item xs={4} style={{height: '30%'}}>
